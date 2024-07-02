fd2460751fdf0a883ce83b33283a5dc76d08a55b
somuthink <qould12@gmail.com>
2024-07-02 19:30:49 +0300

    initial commit

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("tinkoff calculator")
+        print("tinkoff calculator init")
     }
     
     
